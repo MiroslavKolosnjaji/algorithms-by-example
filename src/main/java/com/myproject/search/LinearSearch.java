9cda639cc1c3fe6db4aa27d03d4568108cdfe219
Miroslav <kolosnjaji@stcable.net>
2024-12-10 20:54:21 +0100

    Implemented the 'LinearSearchTest' class, covering linear search method. Closes #7

--- src/main/java/com/myproject/search/LinearSearch.java
+++ src/main/java/com/myproject/search/LinearSearch.java
@@ -5,9 +5,21 @@ package com.myproject.search;
  */
 public class LinearSearch {
 
+    /**
+     *  Linear Search simply goes through each element in the array one by one until it finds the target element.
+     *  Although easy to implement, it has a time complexity of O(n), making it less efficient for large arrays.
+     *  The array does not need to be sorted.
+     *
+     *
+     * @param array (does not need to be sorted)
+     * @param value (no restrictions)
+     * @return value or -1
+     */
+
     public static int linearSearch(int[] array, int value) {
+
         for (int i = 0; i < array.length; i++)
-            if (array[i] == value) return i;
+            if (array[i] == value) return array[i];
 
         return -1;
     }
