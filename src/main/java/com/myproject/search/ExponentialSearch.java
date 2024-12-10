28bd89294e28c53b8b2957896f81945f211453b6
Miroslav <kolosnjaji@stcable.net>
2024-12-10 20:52:40 +0100

    Implemented the 'ExponentialSearchTest' class, covering exponential search method. Closes #5

--- src/main/java/com/myproject/search/ExponentialSearch.java
+++ src/main/java/com/myproject/search/ExponentialSearch.java
@@ -7,6 +7,17 @@ import static com.myproject.search.BinarySearch.binarySearchRec;
  */
 public class ExponentialSearch {
 
+    /**
+     * Exponential Search uses a method similar to Binary Search, but first tries to find a range in which the target element might be located by exponentially increasing the range size.
+     * Once the appropriate range is found, Binary Search is applied within that range. This algorithm is useful when the size of the array is unknown.
+     * The array must be sorted.
+     *
+     * @param array (must be sorted)
+     * @param value (no restrictions)
+     * @return value or -1
+     */
+
+
     public static int exponentialSearch(int[] array, int value){
         int bound = 1;
         while(bound < array.length && array[bound] < value)
--- src/test/java/com/myproject/search/ExponentialSearchTest.java
+++ src/test/java/com/myproject/search/ExponentialSearchTest.java
@@ -1,7 +1,50 @@
+package com.myproject.search;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.*;
+
 /**
-@author Miroslav Kološnjaji
-*/
-    class ExponentialSearchTest {
-  
+ * @author Miroslav Kološnjaji
+ */
+class ExponentialSearchTest {
+
+    @DisplayName("Exponential Search")
+    @Test
+    void testExponentialSearch_whenValueExistsInProvidedArray_returnsSameValue() {
+
+        int[] array = getLongArray(false);
+        int number = 563;
+
+        int result = ExponentialSearch.exponentialSearch(array, number);
+
+        assertEquals(number, result, "Result doesn't match expected value.");
+    }
+
+    @DisplayName("Exponential Search - Value Not Found")
+    @Test
+    void testExponentialSearch_whenValueDoesntExistsInProvidedArray_returnsNegativeValue() {
+
+        int[] array = getLongArray(true);
+        int number = 563;
+
+        int result = ExponentialSearch.exponentialSearch(array, number);
+
+        assertEquals(-1, result, "Result doesn't match expected value.");
+    }
+
+    private int[] getLongArray(boolean hide563) {
+
+        int[] array = new int[1001];
+
+        for (int i = 0; i <= 1000; i++) {
+
+            if (i == 563 && hide563) continue;
+
+            array[i] = i;
+        }
+
+        return array;
+    }
 }
