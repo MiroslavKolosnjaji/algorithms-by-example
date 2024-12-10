fc960911d20f3ed63fb3b357db1c2bae59f632f3
Miroslav <kolosnjaji@stcable.net>
2024-12-10 20:53:43 +0100

    Implemented the 'JumpSearchTest' class, covering jump search method. Closes #6

--- src/main/java/com/myproject/search/JumpSearch.java
+++ src/main/java/com/myproject/search/JumpSearch.java
@@ -5,6 +5,15 @@ package com.myproject.search;
  */
 public class JumpSearch {
 
+    /**
+     * Jump Search divides the array into blocks and searches these blocks by jumping ahead, then performs a linear search within the block where the target element is found.
+     * It is more efficient than Linear Search but not as efficient as Binary Search. The array must be sorted.
+     *
+     * @param array (must be sorted)
+     * @param value (no restrictions)
+     * @return value or -1
+     */
+
     public static int jumpSearch(int[] array, int value) {
         int start = 0;
         int next = array.length / 3;
@@ -18,11 +27,10 @@ public class JumpSearch {
             if (value <= array[next - 1]) {
                 for (int i = start; i < next; i++) {
                     if (array[i] == value)
-                        return i;
+                        return array[i];
                 }
                 break;
             }
-
 
             if (value > array[next - 1]) {
                 start = next;
--- src/test/java/com/myproject/search/JumpSearchTest.java
+++ src/test/java/com/myproject/search/JumpSearchTest.java
@@ -1,7 +1,31 @@
+package com.myproject.search;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.*;
+
 /**
-@author Miroslav Kološnjaji
-*/
-    class JumpSearchTest {
-  
+ * @author Miroslav Kološnjaji
+ */
+class JumpSearchTest {
+
+    int[] array;
+
+    @BeforeEach
+    void setUp() {
+        array = new int[]{1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20};
+    }
+
+    @DisplayName("Jump Search")
+    @Test
+    void testJumpSearch_whenValueExistsInProvidedArray_returnsSameValue() {
+
+        int number = 17;
+
+        int result = JumpSearch.jumpSearch(array, number);
+
+        assertEquals(number, result, "Result doesn't match expected value.");
+    }
 }
