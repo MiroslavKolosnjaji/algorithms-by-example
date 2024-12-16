c21aadf0f2f584467c9b6c8360faee53df9105f2
Miroslav <kolosnjaji@stcable.net>
2024-12-16 06:47:48 +0100

    Created 'BucketSortTest' and 'BubbleSortTest' class: covering all methods and edge cases.

--- src/test/java/com/myproject/sorting/BubbleSortTest.java
+++ src/test/java/com/myproject/sorting/BubbleSortTest.java
@@ -1,7 +1,26 @@
+package com.myproject.sorting;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.*;
+
 /**
-@author Miroslav Kološnjaji
-*/
-    class BubbleSortTest {
-  
+ * @author Miroslav Kološnjaji
+ */
+class BubbleSortTest {
+
+    private final int[] array = {1, 7, 8, 4, 9, 2, 5, 12};
+
+
+    @DisplayName("Bubble Sort")
+    @Test
+    void testBubbleSort_whenValidArrayProvided_returnsSortedArray() {
+
+        int[] expectedArray = {1, 2, 4, 5, 7, 8, 9, 12};
+
+        int[] result = BubbleSort.bubbleSort(array);
+
+        assertArrayEquals(expectedArray, result, "Result doesn't match expected array.");
+    }
 }
--- src/test/java/com/myproject/sorting/BucketSortTest.java
+++ src/test/java/com/myproject/sorting/BucketSortTest.java
@@ -1,7 +1,27 @@
+package com.myproject.sorting;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+
 import static org.junit.jupiter.api.Assertions.*;
+
 /**
-@author Miroslav Kološnjaji
-*/
-    class BucketSortTest {
-  
+ * @author Miroslav Kološnjaji
+ */
+class BucketSortTest {
+
+    private final int[] array = {1, 7, 8, 4, 9, 2, 5, 12};
+
+    @DisplayName("Bucket Sort")
+    @Test
+    void testBucketSort_whenValidArrayProvided_returnsSortedArray() {
+
+        int[] expectedArray = {1, 2, 4, 5, 7, 8, 9, 12};
+
+        int[] result = BucketSort.bucketSort(array);
+
+        assertArrayEquals(expectedArray, result, "Result doesn't match expected array.");
+
+    }
 }
