773b843dfe32f48806250512a7e84f218417bbb0
Miroslav <kolosnjaji@stcable.net>
2024-12-19 15:09:00 +0100

    Created 'QuickSortTest' class: covering all methods and edge cases.

--- src/test/java/com/myproject/sorting/QuickSortTest.java
+++ src/test/java/com/myproject/sorting/QuickSortTest.java
@@ -1,7 +1,27 @@
+package com.myproject.sorting;
+
+import org.junit.jupiter.api.Test;
+
+import java.util.Arrays;
+
 import static org.junit.jupiter.api.Assertions.*;
+
 /**
-@author Miroslav Kološnjaji
-*/
-    class QuickSortTest {
-  
+ * @author Miroslav Kološnjaji
+ */
+class QuickSortTest {
+
+    private final int[] array = {15, 6, 3, 1, 22, 10, 13};
+
+    @Test
+    void testQuickSort_whenValidArrayProvided_returnsSortedArray() {
+
+        int[] expectedArray = {1, 3, 6, 10, 13, 15, 22};
+
+        int[] result = QuickSort.quickSort(array);
+        System.out.println(Arrays.toString(result));
+
+        assertArrayEquals(expectedArray, result, "Result doesn't match expected array.");
+
+    }
 }
