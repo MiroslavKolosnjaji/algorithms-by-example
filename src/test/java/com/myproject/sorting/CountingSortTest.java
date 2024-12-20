02979fe21e8a848eeed72b74cc5f768c78f3fa1d
Miroslav <kolosnjaji@stcable.net>
2024-12-20 14:01:30 +0100

    Implemented 'CountingSortTest' class.

--- src/test/java/com/myproject/sorting/CountingSortTest.java
+++ src/test/java/com/myproject/sorting/CountingSortTest.java
@@ -1,7 +1,27 @@
+package com.myproject.sorting;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.*;
+
 /**
-@author Miroslav Kološnjaji
-*/
-    class CountingSortTest {
-  
+ * @author Miroslav Kološnjaji
+ */
+class CountingSortTest {
+
+    private final int[] array = {1, 7, 8, 4, 9, 2, 5, 12};
+
+
+    @DisplayName("Counting Sort")
+    @Test
+    void testCountingSort_whenValidArrayProvided_returnsSortedArray() {
+
+        int[] expectedArray = {1, 2, 4, 5, 7, 8, 9, 12};
+
+        int[] result = CountingSort.countingSort(array);
+
+        assertArrayEquals(expectedArray, result, "Result doesn't match expected array.");
+    }
 }
